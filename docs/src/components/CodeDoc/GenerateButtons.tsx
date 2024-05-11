c7e1936b431ab3d3b60ce3a9eeff8dde62fa115c
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-11 07:00:15 -0300

    fix: Text content does not match server-rendered HTML

---
+++ docs/src/components/CodeDoc/GenerateButtons.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { generateCnpj, generateCpf } from "cpf_and_cnpj-generator";
+import { useEffect, useState } from "react";
+import { setMask } from "react-input-mask-br";
+
+export function GenerateCpf() {
+	const [cpf, setCpf] = useState("00000000000");
+	const [isCpfMasked, setIsCpfMasked] = useState(true);
+
+	const isMasked = isCpfMasked ? setMask({ type: "cpf", value: cpf }) : cpf;
+
+	const handleCpfMaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setIsCpfMasked(event.target.checked);
+	};
+
+	useEffect(() => {
+		setCpf(generateCpf());
+	}, []);
+	return (
+		<>
+			<button
+				id="gera-cpf"
+				className="rounded bg-blue-950 px-4 py-2 text-white"
+				onClick={() => setCpf(generateCpf())}
+			>
+				Gerar CPF
+			</button>
+			<p className="mt-2">
+				CPF: <span id="response-cpf">{isMasked}</span>
+			</p>
+			<div className="flex items-center gap-2">
+				<label htmlFor="cpf">Use mask ?</label>
+				<input
+					type="checkbox"
+					name="cpf"
+					id="cpf"
+					checked={isCpfMasked}
+					onChange={handleCpfMaskChange}
+				/>
+			</div>
+		</>
+	);
+}
+
+export function GenerateCnpj() {
+	const [cnpj, setCnpj] = useState("00000000000000");
+
+	const [isCnpjMasked, setIsCnpjMasked] = useState(true);
+
+	const isMasked = isCnpjMasked ? setMask({ type: "cnpj", value: cnpj }) : cnpj;
+
+	const handleCnpjMaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setIsCnpjMasked(event.target.checked);
+	};
+
+	useEffect(() => {
+		setCnpj(generateCnpj());
+	}, []);
+	return (
+		<>
+			<button
+				id="gera-cnpj"
+				className="rounded bg-blue-950 px-4 py-2 text-white"
+				onClick={() => setCnpj(generateCnpj())}
+			>
+				Gerar CNPJ
+			</button>
+			<p className="mt-2">
+				CNPJ: <span id="response-cnpj">{isMasked}</span>
+			</p>
+			<div className="flex items-center gap-2">
+				<label htmlFor="cnpj">Use mask ?</label>
+				<input
+					type="checkbox"
+					name="cnpj"
+					id="cnpj"
+					checked={isCnpjMasked}
+					onChange={handleCnpjMaskChange}
+				/>
+			</div>
+		</>
+	);
+}
--- docs/src/components/CodeDoc/index.tsx
+++ docs/src/components/CodeDoc/index.tsx
@@ -1,78 +1,55 @@
-"use client";
-
-import { generateCnpj, generateCpf } from "cpf_and_cnpj-generator";
 import Link from "next/link";
-import { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
+import { GenerateCnpj, GenerateCpf } from "./GenerateButtons";
+
 export default function CodeDoc() {
-  const [cpf, setCpf] = useState(generateCpf());
-  const [cnpj, setCnpj] = useState(generateCnpj());
+	return (
+		<>
+			<SyntaxHighlighter language="bash" style={a11yDark}>
+				npm install cpf_and_cnpj-generator
+			</SyntaxHighlighter>
+			<p className="mb-4">ou</p>
+			<SyntaxHighlighter language="bash" style={a11yDark}>
+				yarn add cpf_and_cnpj-generator
+			</SyntaxHighlighter>
+			<div className="mb-4">
+				<Link
+					href="https://www.npmjs.com/package/cpf_and_cnpj-generator"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="mr-4 text-blue-500 underline"
+				>
+					Go to npm page
+				</Link>
+				<Link
+					href="https://github.com/gabriel-logan/Gerador-CPF-e-CNPJ-valido"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-blue-500 underline"
+				>
+					Go to github page
+				</Link>
+			</div>
 
-  return (
-    <>
-      <SyntaxHighlighter language="bash" style={a11yDark}>
-        npm install cpf_and_cnpj-generator
-      </SyntaxHighlighter>
-      <p className="mb-4">ou</p>
-      <SyntaxHighlighter language="bash" style={a11yDark}>
-        yarn add cpf_and_cnpj-generator
-      </SyntaxHighlighter>
-      <div className="mb-4">
-        <Link
-          href="https://www.npmjs.com/package/cpf_and_cnpj-generator"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4 text-blue-500 underline"
-        >
-          Go to npm page
-        </Link>
-        <Link
-          href="https://github.com/gabriel-logan/Gerador-CPF-e-CNPJ-valido"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 underline"
-        >
-          Go to github page
-        </Link>
-      </div>
+			<h2 className="mb-4 text-2xl font-bold">Como Usar</h2>
+			<div id="div_geradores" className="mb-4">
+				<div className="mx-4 mt-6 flex flex-col gap-2 sm:mx-0 sm:mt-2 sm:flex-row">
+					<GenerateCpf />
+					{/**<span className="cursor-pointer hover:text-blue-950">COPY</span> */}
+				</div>
+				<div className="mx-4 mt-6 flex flex-col gap-2 sm:mx-0 sm:mt-2 sm:flex-row">
+					<GenerateCnpj />
+					{/**<span className="cursor-pointer hover:text-blue-950">COPY</span> */}
+				</div>
+			</div>
+			<p className="mb-4">
+				Após instalar o pacote, você pode utilizá-lo em seu projeto JavaScript:
+			</p>
 
-      <h2 className="mb-4 text-2xl font-bold">Como Usar</h2>
-      <div id="div_geradores" className="mb-4">
-        <div className="mt-2 flex gap-2">
-          <button
-            id="gera-cpf"
-            className="rounded bg-blue-950 px-4 py-2 text-white"
-            onClick={() => setCpf(generateCpf())}
-          >
-            Gerar CPF
-          </button>
-          <p className="mt-2">
-            CPF: <span id="response-cpf">{cpf}</span>
-          </p>
-          {/**<span className="cursor-pointer hover:text-blue-950">COPY</span> */}
-        </div>
-        <div className="mt-2 flex gap-2">
-          <button
-            id="gera-cnpj"
-            className="rounded bg-blue-950 px-4 py-2 text-white"
-            onClick={() => setCnpj(generateCnpj())}
-          >
-            Gerar CNPJ
-          </button>
-          <p className="mt-2">
-            CNPJ: <span id="response-cnpj">{cnpj}</span>
-          </p>
-          {/**<span className="cursor-pointer hover:text-blue-950">COPY</span> */}
-        </div>
-      </div>
-      <p className="mb-4">
-        Após instalar o pacote, você pode utilizá-lo em seu projeto JavaScript:
-      </p>
-
-      <SyntaxHighlighter language="javascript" style={a11yDark}>
-        {`import gerador from 'cpf_and_cnpj-generator';
+			<SyntaxHighlighter language="javascript" style={a11yDark}>
+				{`import gerador from 'cpf_and_cnpj-generator';
 
 console.log(gerador.generateCnpj());
 console.log(gerador.generateCpf());
@@ -92,13 +69,13 @@ const { generateCnpj, generateCpf } = require('cpf_and_cnpj-generator');
 
 console.log('CPF Gerado:', gerador.generateCnpj());
 console.log('CNPJGerado:', gerador.generateCpf());`}
-      </SyntaxHighlighter>
+			</SyntaxHighlighter>
 
-      <SyntaxHighlighter language="javascript" style={a11yDark}>
-        {`// Pode testar o cdn usando
+			<SyntaxHighlighter language="javascript" style={a11yDark}>
+				{`// Pode testar o cdn usando
 <script src="https://unpkg.com/cpf_and_cnpj-generator@1.1.2/dist/bundle.js"></script>
 // ou clicando nos botões na propria pagina`}
-      </SyntaxHighlighter>
-    </>
-  );
+			</SyntaxHighlighter>
+		</>
+	);
 }
