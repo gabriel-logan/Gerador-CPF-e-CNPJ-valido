62df9823d5b42f97967348b9c944e1cc4eab145c
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-05-14 05:20:38 -0300

    feat: using github actions to publish 1.1.3v

--- packages/typescript/src/cnpjGenerator.ts
+++ packages/typescript/src/cnpjGenerator.ts
@@ -6,44 +6,61 @@ function randomDigit() {
 }
 
 // Função para gerar os 12 primeiros dígitos do CNPJ
-function generateCNPJBase() {
+function generateCNPJBase(): number[] {
 	const cnpjBase: number[] = [];
+
 	for (let i: number = 0; i < 12; i++) {
 		cnpjBase.push(randomDigit());
 	}
+
 	return cnpjBase;
 }
 
 // Função para calcular o primeiro dígito verificador
-function calculateFirstVerifier(cnpjBase: number[]) {
+function calculateFirstVerifier(cnpjBase: number[]): number {
 	const weight: number[] = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
 	let sum: number = 0;
+
 	for (let i: number = 0; i < 12; i++) {
 		sum += cnpjBase[i] * weight[i];
 	}
+
 	const remainder: number = sum % 11;
+
 	return remainder < 2 ? 0 : 11 - remainder;
 }
 
 // Função para calcular o segundo dígito verificador
-function calculateSecondVerifier(cnpjBase: number[], firstVerifier: number) {
+function calculateSecondVerifier(
+	cnpjBase: number[],
+	firstVerifier: number,
+): number {
 	const weight: number[] = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
 	let sum: number = 0;
+
 	for (let i: number = 0; i < 12; i++) {
 		sum += cnpjBase[i] * weight[i];
 	}
+
 	sum += firstVerifier * weight[12];
+
 	const remainder: number = sum % 11;
+
 	return remainder < 2 ? 0 : 11 - remainder;
 }
 
 function generateValidCNPJ(): string {
 	const cnpjBase: number[] = generateCNPJBase();
+
 	const firstVerifier: number = calculateFirstVerifier(cnpjBase);
+
 	const secondVerifier: number = calculateSecondVerifier(
 		cnpjBase.concat(firstVerifier),
 		firstVerifier,
 	);
+
 	return `${cnpjBase.join("")}${String(firstVerifier)}${String(secondVerifier)}`;
 }
 
