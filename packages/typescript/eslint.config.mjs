6424dc7e9b89c63bb24d44c4295a328e5e5a3e12
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-06 12:23:29 -0300

    chore: remove deprecated ESLint and EditorConfig files, add new ESLint configuration

---
+++ packages/typescript/eslint.config.mjs
@@ -0,0 +1,47 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "tsconfig.eslint.json",
+        tsconfigRootDir: ".",
+      },
+    },
+    ignores: ["webpack.config.js", "dist/", "types/", "internal/"],
+    rules: {
+      "no-else-return": ["error", { allowElseIf: false }],
+      "consistent-return": "error",
+      "no-console": "warn",
+      "@typescript-eslint/typedef": [
+        "error",
+        {
+          variableDeclaration: true,
+          memberVariableDeclaration: true,
+        },
+      ],
+      "@typescript-eslint/prefer-readonly": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            accessors: "explicit",
+            constructors: "no-public",
+            methods: "explicit",
+            properties: "explicit",
+            parameterProperties: "explicit",
+          },
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/consistent-type-exports": "error",
+    },
+  },
+);
