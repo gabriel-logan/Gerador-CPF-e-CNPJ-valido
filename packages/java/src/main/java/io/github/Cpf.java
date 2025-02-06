1406eb0ea8a8041f16b8d66e14d3a2464ae09ab7
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-06 17:46:19 -0300

    refactor: make CPF array and verifier variables final for better immutability

--- packages/java/src/main/java/io/github/Cpf.java
+++ packages/java/src/main/java/io/github/Cpf.java
@@ -17,28 +17,31 @@ public class Cpf {
     // Function to calculate the verifier digits
     public static int calculateCpfDigit(int[] cpfArray, int factor) {
         int sum = 0;
+
         for (int i = 0; i < cpfArray.length; i++) {
             sum += cpfArray[i] * (factor - i);
         }
+
         return sum % 11 < 2 ? 0 : 11 - (sum % 11);
     }
 
     public static String generateCpf() {
         // Generates an array of 9 random numbers
-        int[] cpfArray = new int[9];
+        final int[] cpfArray = new int[9];
+
         for (int i = 0; i < cpfArray.length; i++) {
             cpfArray[i] = random9Num();
         }
 
         // Calculates the first verifier digit
-        int firstVerifier = calculateCpfDigit(cpfArray, 10);
-        int[] cpfArrayWithFirstVerifier = new int[10];
+        final int firstVerifier = calculateCpfDigit(cpfArray, 10);
+        final int[] cpfArrayWithFirstVerifier = new int[10];
         System.arraycopy(cpfArray, 0, cpfArrayWithFirstVerifier, 0, cpfArray.length);
         cpfArrayWithFirstVerifier[9] = firstVerifier;
 
         // Calculates the second verifier digit
-        int secondVerifier = calculateCpfDigit(cpfArrayWithFirstVerifier, 11);
-        int[] cpfArrayWithBothVerifiers = new int[11];
+        final int secondVerifier = calculateCpfDigit(cpfArrayWithFirstVerifier, 11);
+        final int[] cpfArrayWithBothVerifiers = new int[11];
         System.arraycopy(cpfArrayWithFirstVerifier, 0, cpfArrayWithBothVerifiers, 0, cpfArrayWithFirstVerifier.length);
         cpfArrayWithBothVerifiers[10] = secondVerifier;
 
