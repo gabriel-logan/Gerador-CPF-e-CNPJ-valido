17ae622cc2f62498a0d6aa776c27c055f2335acc
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-06 17:53:59 -0300

    feat: add execution time logging for CPF and CNPJ generation in Main class

--- packages/java/src/main/java/io/github/Main.java
+++ packages/java/src/main/java/io/github/Main.java
@@ -5,8 +5,24 @@ import static java.lang.System.*;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            out.println("CPF: " + Cpf.generateCpf());
-            out.println("CNPJ: " + Cnpj.generateCnpj());
+            long startTimeMs = currentTimeMillis();
+            long startTimeNs = nanoTime();
+            long endTimeMs = currentTimeMillis();
+            long endTimeNs = nanoTime();
+            long durationMs = endTimeMs - startTimeMs;
+            long durationNs = endTimeNs - startTimeNs;
+
+            out.println("CPF: " + Cpf.generateCpf() + " (Time: " + durationMs + " ms, " + durationNs + " ns)");
+
+            startTimeMs = currentTimeMillis();
+            startTimeNs = nanoTime();
+            endTimeMs = currentTimeMillis();
+            endTimeNs = nanoTime();
+            durationMs = endTimeMs - startTimeMs;
+            durationNs = endTimeNs - startTimeNs;
+
+            out.println("CNPJ: " + Cnpj.generateCnpj() + " (Time: " + durationMs + " ms, " + durationNs + " ns)");
+
             out.println();
         }
     }
