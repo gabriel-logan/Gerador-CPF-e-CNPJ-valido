0d2be6296922d81f4fad8c9c8266494e5d734b35
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-06 15:56:13 -0300

    chore: update docs

--- docs/page/_next/static/chunks/app/py/page-b66c2cff44cc45cc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[397],{4787:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=4787)}),_N_E=n.O()}]);
--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -8,6 +8,31 @@ export const metadata: Metadata = {
 	title: "CPF and CNPJ Generator / Gerador de CPF e CNPJ",
 	description:
 		"Este pacote npm fornece funções em JavaScript para gerar números válidos de CPF (Cadastro de Pessoas Físicas) e CNPJ (Cadastro Nacional da Pessoa Jurídica) para o Brasil.",
+	authors: {
+		name: "Gabriel Logan",
+		url: "https://github.com/gabriel-logan",
+	},
+	category: "Library",
+	creator: "Gabriel Logan",
+	keywords: [
+		"cpf",
+		"cnpj",
+		"generator",
+		"gerador",
+		"javascript",
+		"brasil",
+		"cpfGenerator",
+		"cnpjGenerator",
+		"cpf generator",
+		"cnpj generator",
+		"gerador de cpf",
+		"gerador de cnpj",
+		"gerador de cpf e cnpj",
+		"gerador de cpf e cnpj em javascript",
+		"gerador de cpf e cnpj em js",
+		"generateCpf",
+		"generateCnpj",
+	],
 };
 
 export default function RootLayout({
