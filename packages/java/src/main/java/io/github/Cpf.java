8f480a04cd24a3095e6c0525c2741898c126bb9a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-06 23:31:45 -0300

    refactor: update calculateCpfDigit method to accept length parameter for improved flexibility

--- packages/java/src/main/java/io/github/Cpf.java
+++ packages/java/src/main/java/io/github/Cpf.java
@@ -6,10 +6,10 @@ public class Cpf {
     }
 
     // Function to calculate the verifier digits
-    public static byte calculateCpfDigit(byte[] cpfArray, byte factor) {
+    public static byte calculateCpfDigit(byte[] cpfArray, byte length, byte factor) {
         short sum = 0;
 
-        for (byte i = 0; i < 10; i++) {
+        for (byte i = 0; i < length; i++) {
             sum += (short) (cpfArray[i] * (factor - i));
         }
 
@@ -25,10 +25,10 @@ public class Cpf {
         }
 
         // Calculates the first verifier digit
-        cpfArray[9] = calculateCpfDigit(cpfArray, (byte) 10);
+        cpfArray[9] = calculateCpfDigit(cpfArray, (byte) 9, (byte) 10);
 
         // Calculates the second verifier digit
-        cpfArray[10] = calculateCpfDigit(cpfArray, (byte) 11);
+        cpfArray[10] = calculateCpfDigit(cpfArray, (byte) 10, (byte) 11);
 
         // Returns the generated CPF as a string
         return Utils.numberToString(cpfArray);
