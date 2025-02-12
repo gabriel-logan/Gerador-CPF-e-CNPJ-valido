3c3219fd7bf99dffd0227765297f04b4efe6bef4
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-12 02:28:36 -0300

    docs: remove unused assets and update .gitignore for page directory

--- docs/page/_next/static/chunks/app/py/page-fe12b6d805a23e64.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[397],{4981:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=4981)}),_N_E=n.O()}]);
