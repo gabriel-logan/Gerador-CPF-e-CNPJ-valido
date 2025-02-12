29d44314d5f798c23c27567d6f4bb36fbb68575c
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-12 01:32:16 -0300

    refactor: enhance CNPJ and CPF generation logic for improved clarity and efficiency

--- packages/python/src/cpf_and_cnpj_generator/cnpj_generator.py
+++ packages/python/src/cpf_and_cnpj_generator/cnpj_generator.py
@@ -4,28 +4,40 @@ import random
 def random_digit():
     return random.randint(0, 9)
 
-# Função para gerar os 12 primeiros dígitos do CNPJ
-def generate_cnpj_base():
-    cnpj_base = [random_digit() for _ in range(12)]
-    return cnpj_base
-
 # Função para calcular o primeiro dígito verificador
 def calculate_first_verifier(cnpj_base):
     weight = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-    total_sum = sum(cnpj_base[i] * weight[i] for i in range(12))
-    remainder = total_sum % 11
+    
+    _sum = 0
+
+    for i in range(12):
+        _sum += cnpj_base[i] * weight[i]
+    
+    remainder = _sum % 11
+
     return 0 if remainder < 2 else 11 - remainder
 
 # Função para calcular o segundo dígito verificador
 def calculate_second_verifier(cnpj_base, first_verifier):
     weight = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
-    total_sum = sum(cnpj_base[i] * weight[i] for i in range(12))
-    total_sum += first_verifier * weight[12]
-    remainder = total_sum % 11
+    
+    _sum = 0
+
+    for i in range(12):
+        _sum += cnpj_base[i] * weight[i]
+    
+    _sum += first_verifier * weight[12]
+
+    remainder = _sum % 11
+
     return 0 if remainder < 2 else 11 - remainder
 
 def generate_cnpj():
-    cnpj_base = generate_cnpj_base()
-    first_verifier = calculate_first_verifier(cnpj_base)
-    second_verifier = calculate_second_verifier(cnpj_base + [first_verifier], first_verifier)
-    return ''.join(map(str, cnpj_base)) + str(first_verifier) + str(second_verifier)
+    # Cria array de 14 elementos
+    cnpj_base = [random_digit() for _ in range(12)] + [0, 0]
+
+    cnpj_base[12] = calculate_first_verifier(cnpj_base)
+
+    cnpj_base[13] = calculate_second_verifier(cnpj_base, cnpj_base[12])
+
+    return ''.join(map(str, cnpj_base))
--- packages/python/src/cpf_and_cnpj_generator/cpf_generator.py
+++ packages/python/src/cpf_and_cnpj_generator/cpf_generator.py
@@ -4,25 +4,22 @@ def random_num():
     # generates a random number between 0 and 9
     return random.randint(0, 9)
 
-def calculate_digit(cpf_list, factor):
-    # The sum is calculated by multiplying each digit by a factor and summing the results
-    total_sum = sum([val * (factor - idx) for idx, val in enumerate(cpf_list)])
-    # If the sum is less than 2, the verifier digit is 0, otherwise it is 11 minus the remainder of the sum divided by 11
-    return 0 if total_sum % 11 < 2 else 11 - (total_sum % 11)
+def calculate_digit(cpf_list, length, factor):
+    _sum = 0
+
+    for i in range(length):
+        _sum += cpf_list[i] * (factor - i)
+    
+    remainder = _sum % 11
+
+    return 0 if remainder < 2 else 11 - remainder
 
 def generate_cpf():
     # Generates a list of 9 random numbers
-    cpf_list = [random_num() for _ in range(9)]
+    cpf_list = [random_num() for _ in range(9)] + [0, 0]
 
-    # Calculates the first verifier digit
-    first_verifier = calculate_digit(cpf_list, 10)
-    cpf_list.append(first_verifier)
+    cpf_list[9] = calculate_digit(cpf_list, 9, 10)
 
-    # Calculates the second verifier digit
-    second_verifier = calculate_digit(cpf_list, 11)
-    cpf_list.append(second_verifier)
+    cpf_list[10] = calculate_digit(cpf_list, 10, 11)
 
-    # Returns the generated CPF as a string
     return ''.join(map(str, cpf_list))
-
-print(generate_cpf())
