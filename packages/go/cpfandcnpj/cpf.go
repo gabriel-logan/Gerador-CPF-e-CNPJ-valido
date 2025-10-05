eb283f73a05f12d34ee7183cb7c73f8aa396bc8b
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-10-05 01:20:53 -0300

    feat: optimize GenerateCPF function by using uint8 and uint16 for improved performance and clarity

--- packages/go/cpfandcnpj/cpf.go
+++ packages/go/cpfandcnpj/cpf.go
@@ -2,49 +2,44 @@ package cpfandcnpj
 
 import (
 	"math/rand"
-	"strconv"
 )
 
 func GenerateCPF() string {
-	digits := [11]int{}
-
-	sum1 := 0
-	sum2 := 0
+	var digits [11]uint8
+	var sum1, sum2 uint16 // <- to avoid uint8 overflow
 
 	for i := 0; i < 9; i++ {
-		digits[i] = rand.Intn(10)
+		d := uint8(rand.Intn(10))
+		digits[i] = d
 
-		weight1 := 10 - i
-
-		sum1 += digits[i] * weight1
-		sum2 += digits[i] * (weight1 + 1)
+		weights := uint8(10 - i)
+		sum1 += uint16(d * weights)
+		sum2 += uint16(d * (weights + 1))
 	}
 
-	r1 := sum1 % 11
-	dv1 := 0
-	if r1 < 2 {
-		dv1 = 0
-	} else {
+	// Calculate first check digit
+	r1 := uint8(sum1 % 11)
+	dv1 := uint8(0)
+	if r1 >= 2 {
 		dv1 = 11 - r1
 	}
 	digits[9] = dv1
 
-	sum2 += dv1 * 2
+	sum2 += uint16(dv1 * 2)
 
-	r2 := sum2 % 11
-	dv2 := 0
-	if r2 < 2 {
-		dv2 = 0
-	} else {
+	// Calculate second check digit
+	r2 := uint8(sum2 % 11)
+	dv2 := uint8(0)
+	if r2 >= 2 {
 		dv2 = 11 - r2
 	}
 	digits[10] = dv2
 
-	out := ""
+	out := [11]byte{}
 
 	for i := 0; i < 11; i++ {
-		out += strconv.Itoa(digits[i])
+		out[i] = digits[i] + '0'
 	}
 
-	return out
+	return string(out[:])
 }
