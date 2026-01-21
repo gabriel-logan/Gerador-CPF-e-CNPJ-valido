25a5582434b3c15371cafd796c22a20cf3268a63
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-01-21 19:46:28 -0300

    test: update multiform-validator to version 2.8.0-beta and enhance CNPJ generation tests for validation

--- packages/typescript/tests/cnpjGenerator2.test.ts
+++ packages/typescript/tests/cnpjGenerator2.test.ts
@@ -1,7 +1,14 @@
 import cnpjGenerator2 from "../src/cnpjGenerator2";
+import { cnpjIsValid } from "multiform-validator";
 import { describe, it, expect } from "vitest";
 
 describe("geraEValida", () => {
+  it("should generate a valid CNPJ", () => {
+    for (let i: number = 0; i < 100; i++) {
+      expect(cnpjIsValid(cnpjGenerator2(), null, "v2").isValid).toBe(true);
+    }
+  });
+
   it("Should generate a valid cnpj and return a string", () => {
     expect(typeof cnpjGenerator2()).toBe("string");
   });
