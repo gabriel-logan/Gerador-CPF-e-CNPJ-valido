72ab291d865457ca8a4a09e4c75cd9122074c471
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2026-04-28 15:29:39 -0300

    refactor: update charToValue function and improve variable types in GenerateCNPJV2

--- packages/go/cpfandcnpj/cnpjV2.go
+++ packages/go/cpfandcnpj/cnpjV2.go
@@ -9,13 +9,13 @@ import (
 // AlphanumericChars contains the allowed characters for the CNPJ v2 base.
 const AlphanumericChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func charToValue(char byte) int {
+func charToValue(char byte) uint8 {
 	if char >= '0' && char <= '9' {
-		return int(char - '0')
+		return char - '0'
 	}
 
 	if char >= 'A' && char <= 'Z' {
-		return int(char - '0')
+		return char - '0'
 	}
 
 	return 0
@@ -37,36 +37,35 @@ func charToValue(char byte) int {
 func GenerateCNPJV2() Bytes {
 	cnpj := make(Bytes, 14)
 	random := rand.Uint64()
-	sum1 := 0
-	sum2 := 0
+	var sum1, sum2 uint16
 
-	weights1 := [12]int{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2}
-	weights2 := [12]int{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3}
+	weights1 := [12]uint8{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2}
+	weights2 := [12]uint8{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3}
 
 	for i := 0; i < 12; i++ {
-		index := random % uint64(len(AlphanumericChars))
+		index := uint8(random % uint64(len(AlphanumericChars)))
 		random /= uint64(len(AlphanumericChars))
 		cnpj[i] = AlphanumericChars[index]
 
 		value := charToValue(cnpj[i])
-		sum1 += value * weights1[i]
-		sum2 += value * weights2[i]
+		sum1 += uint16(value) * uint16(weights1[i])
+		sum2 += uint16(value) * uint16(weights2[i])
 	}
 
 	remainder := sum1 % 11
-	firstDV := 0
-	if remainder > 1 {
-		firstDV = 11 - remainder
+	firstDV := uint8(0)
+	if remainder >= 2 {
+		firstDV = 11 - uint8(remainder)
 	}
-	cnpj[12] = byte('0' + firstDV)
+	cnpj[12] = firstDV + '0'
 
-	sum2 += firstDV * 2
+	sum2 += uint16(firstDV * 2)
 	remainder = sum2 % 11
-	secondDV := 0
-	if remainder > 1 {
-		secondDV = 11 - remainder
+	secondDV := uint8(0)
+	if remainder >= 2 {
+		secondDV = 11 - uint8(remainder)
 	}
-	cnpj[13] = byte('0' + secondDV)
+	cnpj[13] = secondDV + '0'
 
 	return cnpj
 }
